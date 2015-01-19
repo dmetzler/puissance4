fba988eec52a0cf11d6876f249b0ac14756e98fa
Damien Metzler <dmetzler@nuxeo.com>
2015-01-19 08:04:42 +0100

    Remove custom setup of JAX-RS resources

--- puissance4-rest/src/main/java/org/dmetzler/isen/puissance4/jaxrs/Puissance4Application.java
+++ puissance4-rest/src/main/java/org/dmetzler/isen/puissance4/jaxrs/Puissance4Application.java
@@ -9,12 +9,5 @@ import javax.ws.rs.core.Application;
 @ApplicationPath("/api/*")
 public class Puissance4Application extends Application {
 
-    @Override
-    public Set<Class<?>> getClasses() {
-        Set<Class<?>> classes = new HashSet<>();
-        classes.addAll(super.getClasses());
-        classes.add(Puissance4API.class);
-        classes.add(Puissance4GameBodyWriter.class);
-        return classes;
-    }
+
 }
